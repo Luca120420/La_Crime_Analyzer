729bc6936fad957c188a73995ae7933e53e4b833
Luca120420 <l.lombardi@itsrizzoli.it>
2023-07-06 12:29:05 +0200

    Aggiunta analisi etnia e abbellimento codice

--- CrimeAnalyzer.py
+++ CrimeAnalyzer.py
@@ -30,8 +30,42 @@ class CrimeAnalyzer:
 
     # Pulisco i dati
     def clean_data(self):
-        self.crimes = self.crimes[self.crimes['Vict Age'] != 0]
-        self.crimes['Vict Sex'] = self.crimes['Vict Sex'].replace(['H', 'X'], 'Other')
+        
+        nuove_colonne_diz = {
+            'DR_NO': 'ID',
+            'Date Rptd': 'Data Segn',
+            'DATE OCC': 'Data Avv',
+            'TIME OCC': 'Ora Avv',
+            'AREA': 'Zona',
+            'AREA NAME': 'Nome Zona',
+            'Rpt Dist No': 'Distr Num Segn ',
+            'Part 1-2': 'Part 1-2',
+            'Crm Cd': 'Tipo Crimine Numero',
+            'Crm Cd Desc': 'Tipo Crimine Desc',
+            'Mocodes': 'Modus Operandi',
+            'Vict Age': 'Eta Vittima',
+            'Vict Sex': 'Sesso Vittima',
+            'Vict Descent': 'Etnia Vittima',
+            'Premis Cd': 'Premessa Numero',
+            'Premis Desc': 'Premessa Desc',
+            'Weapon Used Cd': 'Arma Usata Numero',
+            'Weapon Desc': 'Arma Usata Desc',
+            'Status': 'Stato',
+            'Status Desc': 'Stato Desc',
+            'Crm Cd 1': 'Codice Crimine 1',
+            'Crm Cd 2': 'Codice Crimine 2',
+            'Crm Cd 3': 'Codice Crimine 3',
+            'Crm Cd 4': 'Codice Crimine 4',
+            'LOCATION': 'Luogo',
+            'Cross Street': 'Strada',
+            'LAT': 'LAT',
+            'LON': 'LON'
+        }
+
+        self.crimes = self.crimes.rename(columns=nuove_colonne_diz)
+        self.crimes["Etnia Vittima"] = self.crimes["Etnia Vittima"].replace(["H","W","B","O","A","X","K","F","C","J","V","I","Z","P","U","D","G","L","S"],["Ispanico","Caucasico","Di colore","Altro","Asiatico","Sconosciuto","Koreano","Filippino","Cinese","Giapponese","Vietnamita","Nativo Americano","Isolano del Pacifico","Isolano del Pacifico - Samoano","Hawaiano","Cambogiano","Guamaniano","Laotiano","Indo-asiatico"])
+        self.crimes = self.crimes[self.crimes['Eta Vittima'] != 0]
+        self.crimes['Sesso Vittima'] = self.crimes['Sesso Vittima'].replace(['H', 'X'], 'Other')
 
     # Stampo le informazioni del Dataframe
     def print_data_info(self):
@@ -41,56 +75,63 @@ class CrimeAnalyzer:
         print(self.crimes.dtypes)
 
     def analyze_crime_frequency(self):
-        print("Crime Frequency")
-        crime_frequency = self.crimes['Crm Cd Desc'].value_counts()
+        print("Drequenza Crimini")
+        crime_frequency = self.crimes['Tipo Crimine Desc'].value_counts()
         print(crime_frequency)
 
     def analyze_temporal_data(self):
-        self.crimes['Date Rptd'] = pd.to_datetime(self.crimes['Date Rptd'], format='%m/%d/%Y %I:%M:%S %p')
-        self.crimes['Month'] = self.crimes['Date Rptd'].dt.month
-        monthly_crime_count = self.crimes['Month'].value_counts().sort_index()
+        # Trasformiamo la data nel formato %m/%d/%Y %I:%M:%S %p
+        self.crimes['Data Segn'] = pd.to_datetime(self.crimes['Data Segn'], format='%m/%d/%Y %I:%M:%S %p')
+         
+        # Estraiamo il mese e salviamo nella colonna "Mese"
+        self.crimes['Mese'] = self.crimes['Data Segn'].dt.month
+        
+        # Contiamo i valori unici in "Mese" e li ordiniamo
+        monthly_crime_count = self.crimes['Mese'].value_counts().sort_index()
         
         # Uso una list comprehension per trasformare i mesi da numeri a string
         month_names = [calendar.month_name[i] for i in range(1, 13)] 
+        
+        # Assegniamo la lista dei mesi all'indice al posto dei numeri
         monthly_crime_count.index = month_names
 
-        monthly_crime_count.plot(kind='bar', xlabel='Month', ylabel='Number of Crimes')
+        monthly_crime_count.plot(kind='bar', xlabel='Mese', ylabel='Numero di Crimini')
         plt.show()
 
     def analyze_victim_demographics(self):
-        victim_age_distribution = self.crimes['Vict Age'].plot.hist(bins=20)
-        plt.xlabel('Age')
-        plt.ylabel('Frequency')
-        plt.title('Distribution of Victim Ages')
+        victim_age_distribution = self.crimes['Eta Vittima'].plot.hist(bins=20)
+        plt.xlabel('Età')
+        plt.ylabel('Frequenza')
+        plt.title("Distribuzione dell' età delle vittime")
         plt.show()
 
-        victim_gender_distribution = self.crimes['Vict Sex'].value_counts()
+        victim_gender_distribution = self.crimes['Sesso Vittima'].value_counts()
         print(victim_gender_distribution)
 
     def analyze_crime_status(self):
-        crime_status_distribution = self.crimes['Status Desc'].value_counts()
+        crime_status_distribution = self.crimes['Stato Desc'].value_counts()
         print(crime_status_distribution)
 
     def analyze_premise_and_weapon(self):
-        premise_distribution = self.crimes['Premis Desc'].value_counts()
+        premise_distribution = self.crimes['Premessa Desc'].value_counts()
         print(premise_distribution)
 
-        weapon_distribution = self.crimes['Weapon Desc'].value_counts()
+        weapon_distribution = self.crimes['Arma Usata Desc'].value_counts()
         print(weapon_distribution)
 
     def analyze_crime_vs_age(self):
-        crime_vs_age = self.crimes.groupby('Crm Cd Desc')['Vict Age'].mean()
+        crime_vs_age = self.crimes.groupby('Tipo Crimine Desc')['Eta Vittima'].mean()
         print(crime_vs_age)
 
     def analyze_victim_gender_distribution(self):
-        sex_counts = self.crimes['Vict Sex'].value_counts()
+        sex_counts = self.crimes['Sesso Vittima'].value_counts()
 
         labels = sex_counts.index
         counts = sex_counts.values
 
         plt.pie(counts, labels=labels, autopct='%1.1f%%', startangle=90)
         plt.axis('equal')
-        plt.title('Distribution of Crime Victims by Gender')
+        plt.title('Distribuzione del sesso delle vittime')
         plt.show()
         
         
@@ -112,6 +153,15 @@ class CrimeAnalyzer:
 
         # Salviamo la mappa come file html
         crime_map.save('data/crime_map.html')
+        
+        
+        
+    def plot_ethnicity_histogram(self):
+        etnia_count = self.crimes['Etnia Vittima'].value_counts()
+        etnia_count.plot(kind='bar', xlabel='Etnia Vittima', ylabel='Numero di Crimini')
+        plt.title('Distribuzione delle Etnie delle Vittime')
+        plt.show()
+  
 
     def report_file(self):
         
@@ -119,10 +169,10 @@ class CrimeAnalyzer:
         pd.set_option('display.max_rows', None)
         
         report_data = {
-            'Victim Gender Distribution': self.crimes['Vict Sex'].value_counts(),
-            'Crime Status Distribution': self.crimes['Status Desc'].value_counts(),
-            'Premise Distribution': self.crimes['Premis Desc'].value_counts(),
-            'Weapon Distribution': self.crimes['Weapon Desc'].value_counts()
+            'Distribuzione Sesso Vittime': self.crimes['Sesso Vittima'].value_counts(),
+            'Distribuzione Stato dei crimini': self.crimes['Stato Desc'].value_counts(),
+            'Distribuzione delle premesse': self.crimes['Premessa Desc'].value_counts(),
+            'Distribuzione Armi Usate': self.crimes['Arma Usata Desc'].value_counts()
         }
 
         with open(self.report_path, 'w') as file:
@@ -146,6 +196,7 @@ class CrimeAnalyzer:
         self.analyze_premise_and_weapon()
         self.analyze_crime_vs_age()
         self.analyze_victim_gender_distribution()
+        self.plot_ethnicity_histogram()
         self.plot_crime_map()
         self.report_file()
 
